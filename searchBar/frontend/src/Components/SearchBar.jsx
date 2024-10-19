9b86b2f128c3bc5234f2e9807639a8b8aa302a0b
Linux-DEX <sarabjeet.9353@gmail.com>
2024-10-19 19:37:16 +0530

    SearchBar: basic search bar

--- searchBar/frontend/src/Components/SearchBar.jsx
+++ searchBar/frontend/src/Components/SearchBar.jsx
@@ -1,11 +1,71 @@
-import React from "react";
+import "./SearchBar.css";
+import { useState } from "react";
 
 const SearchBar = () => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const countries = [
+    { name: "Belgium", continent: "Europe" },
+    { name: "India", continent: "Asia" },
+    { name: "Bolivia", continent: "South America" },
+    { name: "Ghana", continent: "Africa" },
+    { name: "Japan", continent: "Asia" },
+    { name: "Canada", continent: "North America" },
+    { name: "New Zealand", continent: "Australasia" },
+    { name: "Italy", continent: "Europe" },
+    { name: "South Africa", continent: "Africa" },
+    { name: "China", continent: "Asia" },
+    { name: "Paraguay", continent: "South America" },
+    { name: "Usa", continent: "North America" },
+    { name: "France", continent: "Europe" },
+    { name: "Botswana", continent: "Africa" },
+    { name: "Spain", continent: "Europe" },
+    { name: "Senegal", continent: "Africa" },
+    { name: "Brazil", continent: "South America" },
+    { name: "Denmark", continent: "Europe" },
+    { name: "Mexico", continent: "South America" },
+    { name: "Australia", continent: "Australasia" },
+    { name: "Tanzania", continent: "Africa" },
+    { name: "Bangladesh", continent: "Asia" },
+    { name: "Portugal", continent: "Europe" },
+    { name: "Pakistan", continent: "Asia" },
+  ];
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
+
   return (
-    <>
-      <h1>Search component</h1>
-      <input type="text" />
-    </>
+    <div className="search-container">
+      <h1>basic Search bar</h1>
+      <input
+        type="search"
+        placeholder="search here"
+        onChange={handleChange}
+        value={searchInput}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Country</th>
+            <th>Continent</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredCountries.map((country, index) => (
+            <tr key={index}>
+              <td>{country.name}</td>
+              <td>{country.continent}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
